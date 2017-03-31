7a7ca26ab56eee0d45766c6fafff2427eff0acb9
Max Britvin <maksbritvin@gmail.com>
2017-03-31 19:18:03 +0300

    Create new table

--- database_create.py
+++ database_create.py
@@ -12,10 +12,14 @@ import os.path
 
 con = lite.connect('test.db')
 cur = con.cursor()
-cur.execute('''CREATE TABLE "Hierarchies" ("id" INTEGER PRIMARY KEY,
-                  "Name" TEXT NOT NULL);''')
-cur.execute('''CREATE TABLE "Entity"("id" INTEGER PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT, 
+cur.execute('''CREATE TABLE IF NOT EXISTS "Hierarchies"  ("id" INTEGER PRIMARY KEY,"Name" TEXT NOT NULL);''')
+cur.execute('''CREATE TABLE IF NOT EXISTS "Entity"("id" INTEGER PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT, 
                       "Currency" TEXT, "AllowAdjs" TEXT, "AllowAdjFromChildren" TEXT, "HoldingCompany" TEXT,
                       "SecurityAsPartner" TEXT, "IsICP" TEXT, "SecurityClass" TEXT, "UserDefined1" TEXT,
                       "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+cur.execute(
+    '''CREATE TABLE IF NOT EXISTS "MovProd" ("id" INTEGER PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT, 
+                        "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT, 
+                        "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                        "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
 con.commit()
