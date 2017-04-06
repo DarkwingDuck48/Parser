8309c1da1bef1f0ab59429bb9eef966624c5a081
Max Britvin <maksbritvin@gmail.com>
2017-04-06 16:45:06 +0300

    add function for base elements

--- Parser_Ads.py
+++ Parser_Ads.py
@@ -6,11 +6,13 @@ class AdsParse(object):
         self.ads_file = ads_file
         self.tablename = None
         self.columns = None
-        self.data = {}          # Store all data from ads, key - table name, value - data
-        self.data_str = []
+        # Store all data from ads, key - table name, value - data
+        self.data = self.parse()
         self.customtop = ['MovProd', 'VarLob', 'MktOvr', 'AuditDim', 'RelPartDisc1', 'CostCenterDisc2', 'CustType']
 
     def parse(self):
+        data = {}
+        data_str = []
         with open(self.ads_file, 'r', encoding='utf-8') as meta:
             for line in meta:
                 if line.startswith("!Hierarchies"):  # or line.startswith('!Section'):
@@ -20,13 +22,14 @@ class AdsParse(object):
                     for i in range(0, len(self.columns)):
                         if self.columns[i].startswith("Alias"):
                             self.columns[i] = self.columns[i].strip().split("=")[0]
-                    self.data_str.append(tuple(self.columns))
+                    data_str.append(tuple(self.columns))
                 elif not line.isspace():
-                    self.data_str.append(line.strip().split(";"))
+                    data_str.append(line.strip().split(";"))
                 else:
                     if self.tablename:
-                        self.data.update({self.tablename: self.data_str})
-                    self.data_str = []
+                        data.update({self.tablename: data_str})
+                    data_str = []
+        return data
 
 
 def insertvalues (tablename, values):
@@ -59,23 +62,19 @@ def insertvalues (tablename, values):
                 cur.executemany("INSERT INTO %s VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);" % tablename, (data,))
             con.commit()
 
+
+def base(tablename, parentname):
+    con = lite.connect('test.db')
+    cur = con.cursor()
+    selectedvalue = list(cur.execute("SELECT Child from %s WHERE Parent = (?)" % tablename, (parentname,)))
+    return selectedvalue
+
 if __name__ == '__main__':
     test = AdsParse('GRSHFM_Metadata_17030102.ads')
-    test.parse()
-    #name = enumerate(test.data.keys(), start=1)
-    #data_entity = list(enumerate(test.data['Entity'], start=0))
-    #data_account = list(enumerate(test.data['Account'], start=0))
-    #data_movprod = list(enumerate(test.data['MovProd'], start=0))
-    #data_varlob = list(enumerate(test.data['VarLob'], start=0))
-    #data_mktovr = list(enumerate(test.data['MktOvr'], start=0))
+    '''
     for key in test.data.keys():
         data = list(enumerate(test.data[key], start=0))
         insertvalues(key, data)
     print("Tables is filled")
-
-    #print(test.data["MktOvr"][0])
-    #insertvalues("Entity", data_entity)
-    #insertvalues("MovProd", data_movprod)
-    #insertvalues("Account", data_account)
-    #insertvalues("VarLob", data_varlob)
-    #insertvalues("MktOvr", data_mktovr)
+    '''
+    print(base('Accounts', '1110000'))
