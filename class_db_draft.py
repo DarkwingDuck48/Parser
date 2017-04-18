1b91f978bbd1fa89ebc5f5d816ec49b5841502f6
Max Britvin <maksbritvin@gmail.com>
2017-04-18 17:48:31 +0300

    add create_db in class methods

--- class_db_draft.py
+++ class_db_draft.py
@@ -2,9 +2,17 @@ import sqlite3 as lite
 import os
 import os.path
 
+
 class Metadata(object):
     def __init__(self, databasename, ads_file):
+        '''
+        
+        :param databasename: name of database without extention
+        :param ads_file: ads file with GRSHFM metadata
+        :parameter database_created: status of creation database 
+        '''
         self.databasename = None
+        self.database_created = False
         self.ads_file = ads_file
         err = databasename.split(".")
         if len(err) == 1:
@@ -13,11 +21,23 @@ class Metadata(object):
         else:
             raise ValueError("You need to enter only name of file")
 
+        self.data = self.parse()
+        self.database_created = self.create_database()
+
+        if self.data and self.database_created:
+            for tablename in self.data.keys():
+                data = list(enumerate(self.data[tablename], start=0))
+                self.insertvalues(tablename, data)
+
     def parse(self):
+        '''
+        
+        :return: dictionary with parsed metadata
+        '''
         tablename = None
         data = {}
         data_str = []
-        with open(ads_file, 'r', encoding='utf-8') as meta:
+        with open(self.ads_file, 'r', encoding='utf-8') as meta:
             for line in meta:
                 if line.startswith("!Hierarchies"):  # or line.startswith('!Section'):
                     tablename = line.strip().split('=')[1]
@@ -35,8 +55,108 @@ class Metadata(object):
                     data_str = []
         return data
 
-    def insertvalues (self):
-        con = lite.connect(db)
+    def create_database(self):
+        con = lite.connect(self.databasename)
+        with con:
+            cur = con.cursor()
+
+            # Entity
+            cur.execute('''CREATE TABLE IF NOT EXISTS "Entity"("id" INTEGER PRIMARY KEY, "Parent" TEXT, "Child" TEXT,
+                        "IsPrimary" TEXT, "Currency" TEXT, "AllowAdjs" TEXT, "AllowAdjFromChildren" TEXT, 
+                        "HoldingCompany" TEXT, "SecurityAsPartner" TEXT, "IsICP" TEXT, "SecurityClass" TEXT, 
+                        "UserDefined1" TEXT,"UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+            # Account
+            cur.execute(
+                '''CREATE TABLE IF NOT EXISTS "Accounts" (
+            "id" INTEGER PRIMARY KEY,
+            "Parent" TEXT,
+            "Child" TEXT,
+            "IsPrimary" TEXT,
+            "ConsolidationAccountType" TEXT,
+            "IsCalculated" TEXT,
+            "IsConsolidated" TEXT,
+            "PlugAccount" TEXT,
+            "CustomTopMemberAuditDim" TEXT DEFAULT '[None]',
+            "CustomTopMemberCostCenterDisc2" TEXT DEFAULT '[None]',
+            "CustomTopMemberMktOvr" TEXT DEFAULT '[None]',
+            "CustomTopMemberMovProd" TEXT DEFAULT '[None]',
+            "CustomTopMemberRelPartDisc1" TEXT DEFAULT '[None]',
+            "CustomTopMemberVarLob" TEXT DEFAULT '[None]',
+            "CustomTopMemberCustType" TEXT DEFAULT '[None]',
+            "NumDecimalPlaces" INTEGER,
+            "EnableCustomAggrAuditDim" TEXT,
+            "EnableCustomAggrCostCenterDisc2" TEXT,
+            "EnableCustomAggrMktOvr" TEXT,
+            "EnableCustomAggrMovProd" TEXT,
+            "EnableCustomAggrRelPartDisc1" TEXT,
+            "EnableCustomAggrVarLob" TEXT,
+            "EnableCustomAggrCustType" TEXT,
+            "XBRLTags" TEXT,
+            "ICPTopMember" TEXT,
+            "IsICP" TEXT,
+            "UsesLineItems" TEXT,
+            "SecurityClass" TEXT,
+            "EnableDataAudit" TEXT,
+            "CalcAttribute" TEXT,
+            "SubmissionGroup" TEXT,
+            "UserDefined1" TEXT,
+            "UserDefined2" TEXT,
+            "UserDefined3" TEXT,
+            "Alias" TEXT);''')
+
+            # MovProd
+            cur.execute(
+                '''CREATE TABLE IF NOT EXISTS "MovProd" ("id" INTEGER PRIMARY KEY, "Parent" TEXT, "Child" TEXT, 
+                      "IsPrimary" TEXT, "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                      "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                      "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+            # VarLob
+            cur.execute(
+                ''' CREATE TABLE IF NOT EXISTS "VarLob" ("id" PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT,
+                                "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                                "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                                "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+            # MktOvr
+            cur.execute(
+                ''' CREATE TABLE IF NOT EXISTS "MktOvr" ("id" PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT,
+                                "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                                "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                                "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+            # AuditDim
+            cur.execute(
+                ''' CREATE TABLE IF NOT EXISTS "AuditDim" ("id" PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT,
+                                "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                                "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                                "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+            # RelPartDisc1
+            cur.execute(
+                ''' CREATE TABLE IF NOT EXISTS "RelPartDisc1" ("id" PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT,
+                                "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                                "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                                "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+            # CostCenterDisc2
+            cur.execute(
+                ''' CREATE TABLE IF NOT EXISTS "CostCenterDisc2" ("id" PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT,
+                                "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                                "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                                "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+
+            # CustType
+            cur.execute(
+                ''' CREATE TABLE IF NOT EXISTS "CustType" ("id" PRIMARY KEY, "Parent" TEXT, "Child" TEXT, "IsPrimary" TEXT,
+                                "AggregationWeight" TEXT, "SwitchSignForFlow" TEXT, "SwitchTypeForFlow" TEXT,
+                                "SecurityClass" TEXT, "IsCalculated" TEXT, "SubmissionGroup" TEXT, "UserDefined1" TEXT,
+                                "UserDefined2" TEXT, "UserDefined3" TEXT, "Alias" TEXT);''')
+        return True
+
+    def insertvalues(self, tablename, values):
+        '''
+        
+        :param tablename: Name of dimension
+        :param values: data from metadata
+        :return: 
+        '''
+        con = lite.connect(self.databasename)
         with con:
             cur = con.cursor()
             if tablename == 'Entity':
@@ -51,14 +171,17 @@ class Metadata(object):
                     for x in values[i][1]:
                         data.append(x)
                     cur.executemany('''INSERT OR REPLACE INTO Accounts VALUES
-                                       (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);''', (data,))
-            elif tablename in ['MovProd', 'VarLob', 'MktOvr', 'AuditDim', 'RelPartDisc1', 'CostCenterDisc2', 'CustType']:
+                                  (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);''', (data,))
+            elif tablename in ['MovProd', 'VarLob', 'MktOvr', 'AuditDim', 'RelPartDisc1', 'CostCenterDisc2',
+                               'CustType']:
                 for i in range(1, len(values)):
                     data = [values[i][0]]
                     for x in values[i][1]:
                         data.append(x)
-                    cur.executemany("INSERT OR REPLACE INTO %s VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);" % tablename, (data,))
+                    cur.executemany('''INSERT OR REPLACE INTO %s 
+                                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);''' % tablename, (data,))
+        return True
 
 if __name__ == '__main__':
-    test = Metadata('join_test,txt','GRSHFM_Metadata_17040101.ads')
-    print(test.databasename)
+    test = Metadata('classtest', 'GRSHFM_Metadata_17040101.ads')
+
